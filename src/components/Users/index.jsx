4e02aaf9e5fd63bcfaf6771b8fcfd2a230225814
ianokt0 <ian.gutho16@gmail.com>
2023-11-18 16:51:49 +0700

    update UI

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Users = async ({ api }) => {
     return (
         <div className="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 mt-4">
-            <h5 className="mb-4 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 text-center">Fetch Api From www.indonesiamitramedia.com/api/v1</h5>
+            <h5 className="mb-4 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 text-center">{api.message}</h5>
             <hr />
             <div className="flex flex-col">
                 <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -37,6 +37,16 @@ const Users = async ({ api }) => {
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="mt-2">
+                <span>
+                    fetch api
+                    <Link href="https://indonesiamitramedia.com" className="ml-1 underline text-color-accent hover:text-color-primary">
+                        <i>
+                            www.indonesiamitramedia.com
+                        </i>
+                    </Link>
+                </span>
             </div>
         </div>
     )
